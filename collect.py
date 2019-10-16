a3fa908deb36ad8aa73997cb59308ea1647fcd2f
James-Charles-Robinson <54746146+James-Charles-Robinson@users.noreply.github.com>
2019-10-16 19:27:37 +0100

    Update collect.py

--- collect.py
+++ collect.py
@@ -4,13 +4,14 @@ from PIL import Image, ImageFilter
 import glob
 import imutils
 import random
-import shutil,os
+import shutil
+import os
 
 '''
 Collects faces from photos and videos and saves in faces folder
 '''
 
-#cv2 cascades
+# cv2 cascades
 face_cascade1 = cv2.CascadeClassifier(
     "cascades/data/haarcascade_frontalface_alt2.xml")
 face_cascade2 = cv2.CascadeClassifier(
@@ -18,7 +19,8 @@ face_cascade2 = cv2.CascadeClassifier(
 face_cascade3 = cv2.CascadeClassifier(
     "cascades/data/haarcascade_frontalface_alt.xml")
 
-#moves files from input to done
+
+# moves files from input to done
 def Move():
     for filename in glob.glob('input/*'):
         try:
@@ -27,17 +29,18 @@ def Move():
             print (e)
             os.remove(filename)
 
-#detects faces in an images
+
+# detects faces in an images
 def GetFace(image):
     faces = []
     faces1 = face_cascade1.detectMultiScale(image,
-                                        scaleFactor=1.5, minNeighbors=5)
+                                            scaleFactor=1.5, minNeighbors=5)
     faces2 = face_cascade2.detectMultiScale(image,
-                                        scaleFactor=1.5, minNeighbors=5)
+                                            scaleFactor=1.5, minNeighbors=5)
     faces3 = face_cascade3.detectMultiScale(image,
-                                        scaleFactor=1.5, minNeighbors=5)
-    
-    #only get one group of faces faces
+                                            scaleFactor=1.5, minNeighbors=5)
+
+    # only get one group of faces faces
     if len(faces1) > 0:
         faces = faces1
     elif len(faces2) > 0:
@@ -45,10 +48,11 @@ def GetFace(image):
     elif len(faces3) > 0:
         faces = faces3
     return faces
-    
-#saves the face in the face folder
+
+
+# saves the face in the face folder
 def SaveFace(i, roi):
-    
+
     name = "faces/face-" + str(i) + ".png"
     cv2.imwrite(name, roi)
 
@@ -56,53 +60,54 @@ def SaveFace(i, roi):
 # the main function for videos  
 def Videos():
     
-    for filename in glob.glob('input/*.mp4'): #mp4's in input folder
+    for filename in glob.glob('input/*.mp4'):  # mp4's in input folder
 
-        cap = cv2.VideoCapture(filename) #video stored as cap
+        cap = cv2.VideoCapture(filename)  # video stored as cap
         size = (int(cap.get(3)), int(cap.get(4)))
         fps = cap.get(cv2.CAP_PROP_FPS)
         frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
         durr = frame_count/fps
-        frame_i = 0 #frame index
-        last_saved_index = 0 #last index saved
-        last_index = 0 #last face found
-        last_face = 0 #last face position
+        frame_i = 0  # frame index
+        last_saved_index = 0  # last index saved
+        last_index = 0  # last face found
+        last_face = 0  # last face position
         last_colour = [0, 0, 0]
         out_img = []
 
-        #video is max 10 mins
+        # video is max 10 mins
         if durr > 600:
             print("Video Too Long,", durr, "seconds")
             continue
 
-        while (True): #loop for each frame in the vid
+        while (True):  # loop for each frame in the vid
             try:
                 ret, frame = cap.read()
                 grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                #frame in grey scale
+                # frame in grey scale
             except:
-                break #when the videos over
+                break  # when the videos over
 
-            #only checking every 6 frames speeds it up
+            # only checking every 6 frames speeds it up
             if frame_i % 6 == 0:  
-                #get faces using cv2 cascade
+                # get faces using cv2 cascade
                 faces = GetFace(grey)
 
-                #for found faces get boundaries
+                # for found faces get boundaries
                 for (x, y, w, h) in faces:
 
                     roi_colour = frame[y:y+h, x:x+w]
 
-                    #save face every once and a while
+                    # save face every once and a while
                     if frame_i - last_saved_index > 15:
 
-                        SaveFace(frame_i+random.randint(100, 10000), roi_colour)
+                        SaveFace(
+                            frame_i+random.randint(100, 10000),
+                            roi_colour)
                         last_saved_index = frame_i
 
                     last_face = (x, y, w, h)
                     last_index = frame_i
 
-    
             frame_i += 1
             if frame_i % 10 == 0:
                 print(frame_i)
@@ -111,21 +116,23 @@ def Videos():
         
     cv2.destroyAllWindows()
 
+
 def Photos():
 
-    for filename in glob.glob('input/*'): #everything else in input folder
+    for filename in glob.glob('input/*'):  # everything else in input folder
         if "jpg" in filename or "png" in filename:
 
             im = cv2.imread(filename)
             try:
-                grey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY) #frame in grey scale
+                grey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+                # frame in grey scale
             except:
                 pass
             
-            #get faces using cv2 cascade
+            # get faces using cv2 cascade
             faces = GetFace(grey)
 
-            #for found faces get boundaries
+            # for found faces get boundaries
             for (x, y, w, h) in faces:
 
                 roi_colour = im[y:y+h, x:x+w]
